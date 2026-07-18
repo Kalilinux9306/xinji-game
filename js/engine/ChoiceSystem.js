ff2ea8c5fcbdcb1aefdc07287840cec060cffbc1
Kalilinux9306 <buyi1633160n@163.com>
2026-07-18 13:24:43 +0800

    Update js/engine/ChoiceSystem.js

--- js/engine/ChoiceSystem.js
+++ js/engine/ChoiceSystem.js
@@ -1,6 +1,7 @@
 class ChoiceSystem {
-    constructor(containerId) {
+    constructor(containerId, audioManager) {
         this.container = document.getElementById(containerId);
+        this.audio = audioManager;
     }
 
     show(choices, onSelect) {
@@ -11,16 +12,41 @@ class ChoiceSystem {
             const btn = document.createElement('button');
             btn.className = 'choice-btn';
             btn.textContent = choice.text;
-            btn.addEventListener('click', () => {
-                this.hide();
-                onSelect(choice, index);
+            btn.style.animationDelay = `${index * 0.1}s`;
+
+            btn.addEventListener('mouseenter', () => {
+                btn.classList.add('choice-hover');
+                if (this.audio) this.audio.playSFX('hover');
             });
+
+            btn.addEventListener('mouseleave', () => {
+                btn.classList.remove('choice-hover');
+            });
+
+            btn.addEventListener('click', () => {
+                if (this.audio) this.audio.playSFX('click');
+                btn.classList.add('choice-click');
+                setTimeout(() => {
+                    this.hide();
+                    onSelect(choice, index);
+                }, 200);
+            });
+
             this.container.appendChild(btn);
         });
+
+        this.container.classList.add('choices-enter');
+        setTimeout(() => {
+            this.container.classList.remove('choices-enter');
+        }, 500);
     }
 
     hide() {
-        this.container.classList.add('hidden');
-        this.container.innerHTML = '';
+        this.container.classList.add('choices-exit');
+        setTimeout(() => {
+            this.container.classList.add('hidden');
+            this.container.classList.remove('choices-exit');
+            this.container.innerHTML = '';
+        }, 300);
     }
 }
