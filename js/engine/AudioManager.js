8741d2a0459dbaf4de315f38c338c44b5cb90e45
Kalilinux9306 <buyi1633160n@163.com>
2026-07-18 14:45:59 +0800

    Update js/engine/AudioManager.js

--- js/engine/AudioManager.js
+++ js/engine/AudioManager.js
@@ -104,33 +104,47 @@ class AudioManager {
         if (!ctx) return null;
 
         const chordProgressions = {
-            1: [[261.63, 329.63, 392.00], [293.66, 349.23, 440.00], [329.63, 392.00, 493.88], [293.66, 349.23, 440.00]],
-            2: [[293.66, 349.23, 440.00], [329.63, 392.00, 493.88], [349.23, 440.00, 523.25], [329.63, 392.00, 493.88]],
-            3: [[329.63, 392.00, 493.88], [349.23, 440.00, 523.25], [392.00, 493.88, 587.33], [349.23, 440.00, 523.25]],
-            4: [[392.00, 493.88, 587.33], [440.00, 523.25, 659.25], [493.88, 587.33, 739.99], [440.00, 523.25, 659.25]],
-            5: [[329.63, 392.00, 493.88], [349.23, 440.00, 523.25], [293.66, 349.23, 440.00], [261.63, 329.63, 392.00]],
-            6: [[293.66, 349.23, 440.00], [261.63, 329.63, 392.00], [246.94, 293.66, 369.99], [220.00, 261.63, 329.63]],
-            7: [[220.00, 261.63, 329.63], [196.00, 246.94, 293.66], [174.61, 220.00, 261.63], [196.00, 246.94, 293.66]],
-            8: [[329.63, 392.00, 493.88], [392.00, 440.00, 587.33], [440.00, 523.25, 659.25], [523.25, 659.25, 783.99]]
+            1: [[523.25, 659.25, 783.99], [587.33, 739.99, 880.00], [659.25, 783.99, 987.77], [587.33, 739.99, 880.00]],
+            2: [[587.33, 739.99, 880.00], [659.25, 783.99, 987.77], [698.46, 880.00, 1046.50], [659.25, 783.99, 987.77]],
+            3: [[659.25, 783.99, 987.77], [698.46, 880.00, 1046.50], [783.99, 987.77, 1174.66], [698.46, 880.00, 1046.50]],
+            4: [[783.99, 987.77, 1174.66], [880.00, 1046.50, 1318.51], [987.77, 1174.66, 1479.98], [880.00, 1046.50, 1318.51]],
+            5: [[659.25, 783.99, 987.77], [698.46, 880.00, 1046.50], [587.33, 739.99, 880.00], [523.25, 659.25, 783.99]],
+            6: [[587.33, 739.99, 880.00], [523.25, 659.25, 783.99], [493.88, 622.25, 749.19], [440.00, 523.25, 659.25]],
+            7: [[440.00, 523.25, 659.25], [392.00, 493.88, 587.33], [349.23, 440.00, 523.25], [392.00, 493.88, 587.33]],
+            8: [[659.25, 783.99, 987.77], [783.99, 880.00, 1174.66], [880.00, 1046.50, 1318.51], [1046.50, 1318.51, 1567.98]]
+        };
+
+        const melodyNotes = {
+            1: [659.25, 783.99, 880.00, 783.99, 659.25, 587.33, 523.25],
+            2: [783.99, 880.00, 987.77, 880.00, 783.99, 659.25, 587.33],
+            3: [880.00, 987.77, 1046.50, 987.77, 880.00, 783.99, 659.25],
+            4: [987.77, 1046.50, 1174.66, 1046.50, 987.77, 880.00, 783.99],
+            5: [783.99, 659.25, 587.33, 659.25, 783.99, 880.00, 783.99],
+            6: [587.33, 523.25, 493.88, 523.25, 587.33, 659.25, 587.33],
+            7: [523.25, 440.00, 392.00, 440.00, 523.25, 587.33, 523.25],
+            8: [880.00, 987.77, 1046.50, 1174.66, 1318.51, 1174.66, 987.77]
         };
 
         const progression = chordProgressions[chapter] || chordProgressions[1];
-        const chordInterval = 4000;
+        const melody = melodyNotes[chapter] || melodyNotes[1];
+        const chordInterval = 1500;
+        const noteInterval = 200;
         
         let chordIndex = 0;
+        let melodyIndex = 0;
         
         const playChord = (chord, time) => {
             chord.forEach((freq, idx) => {
                 const osc = ctx.createOscillator();
                 const gain = ctx.createGain();
                 
-                osc.type = 'triangle';
+                osc.type = 'sine';
                 osc.frequency.value = freq;
                 
-                const delay = idx * 0.03;
+                const delay = idx * 0.02;
                 gain.gain.setValueAtTime(0, time + delay);
-                gain.gain.linearRampToValueAtTime(this.bgmVolume * 0.15, time + delay + 0.1);
-                gain.gain.exponentialRampToValueAtTime(0.005, time + chordInterval / 1000);
+                gain.gain.linearRampToValueAtTime(this.bgmVolume * 0.08, time + delay + 0.05);
+                gain.gain.exponentialRampToValueAtTime(0.001, time + chordInterval / 1000);
                 
                 osc.connect(gain);
                 gain.connect(ctx.destination);
@@ -138,23 +152,24 @@ class AudioManager {
                 osc.start(time + delay);
                 osc.stop(time + chordInterval / 1000);
             });
+        };
+
+        const playMelodyNote = (note, time) => {
+            const osc = ctx.createOscillator();
+            const gain = ctx.createGain();
             
-            const melodyNote = chord[0] * 2;
-            const melodyOsc = ctx.createOscillator();
-            const melodyGain = ctx.createGain();
+            osc.type = 'sine';
+            osc.frequency.value = note;
             
-            melodyOsc.type = 'sine';
-            melodyOsc.frequency.value = melodyNote;
+            gain.gain.setValueAtTime(0, time);
+            gain.gain.linearRampToValueAtTime(this.bgmVolume * 0.12, time + 0.03);
+            gain.gain.exponentialRampToValueAtTime(0.001, time + noteInterval / 1000 - 0.02);
             
-            melodyGain.gain.setValueAtTime(0, time + 0.5);
-            melodyGain.gain.linearRampToValueAtTime(this.bgmVolume * 0.1, time + 0.6);
-            melodyGain.gain.exponentialRampToValueAtTime(0.005, time + chordInterval / 1000 - 0.5);
+            osc.connect(gain);
+            gain.connect(ctx.destination);
             
-            melodyOsc.connect(melodyGain);
-            melodyGain.connect(ctx.destination);
-            
-            melodyOsc.start(time + 0.5);
-            melodyOsc.stop(time + chordInterval / 1000);
+            osc.start(time);
+            osc.stop(time + noteInterval / 1000);
         };
 
         const schedule = () => {
@@ -164,7 +179,13 @@ class AudioManager {
             const chord = progression[chordIndex % progression.length];
             playChord(chord, time);
             
+            for (let i = 0; i < 7; i++) {
+                const note = melody[(melodyIndex + i) % melody.length];
+                playMelodyNote(note, time + i * noteInterval / 1000);
+            }
+            
             chordIndex++;
+            melodyIndex += 7;
             this._bgmTimer = setTimeout(schedule, chordInterval);
         };
 
